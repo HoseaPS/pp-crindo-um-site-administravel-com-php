1d998d980ac1484b9e8ea14e3d0d04b4f6b84562
Oséias P. Silva <hoseaps@gmail.com>
2019-07-09 21:36:58 -0300

    declarando arquivo principal para rotas

--- bootstrap.php
+++ bootstrap.php
@@ -1,3 +1,11 @@
 <?php 
 
-echo 'Hello World';
+$path = $_SERVER['PATH_INFO'] ?? '/';
+
+if ($path == '/') {
+    require __DIR__ . '/site/routes.php';
+} elseif ($path == '/teste') {
+    require __DIR__ . '/site/teste.php';
+} else {
+    echo 'Página não encontrada';
+}
