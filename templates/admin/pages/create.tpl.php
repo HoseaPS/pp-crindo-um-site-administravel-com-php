e7b389b5ce18d747bb1d45bbcca9091722b1b0ff
Oséias P. Silva <hoseaps@gmail.com>
2019-08-10 13:19:11 -0300

    template de formulario de cadstro de pagina

--- templates/admin/pages/create.tpl.php
+++ templates/admin/pages/create.tpl.php
@@ -1,3 +1,30 @@
 <h3>Administração de Páginas</h3>
 
-criação de paginas
+<form action="" method="POST">
+    <div class="form-group">
+        <label for="pagesTitle">Título</label>
+        <input name="title" id="pagesTitle" placeholder="Aqui vai o título da página..." type="text" class="form-control">
+    </div>
+
+    <div class="form-group">
+        <label for="pagesUrl">URL</label>
+        <div class="input-group">
+            <div class="input-group-prepend">
+                <span class="input-group-text">/</span>
+            </div>
+            <input type="text" name="url" id="pagesUrl" class="form-control" placeholder="Url amigável, deixe em branco para informar a página inicial...">
+        </div>
+    </div>
+
+    <div class="form-group">
+        //editor de texto
+    </div>
+    
+    <button type="submit" class="btn btn-primary">
+        Salvar
+    </button>
+</form>
+
+<hr>
+
+<a class="btn btn-secondary" href="/admin/pages">voltar</a>
