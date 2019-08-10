364e008d9ca5724baab4e20bb29746a484d8365d
Oséias P. Silva <hoseaps@gmail.com>
2019-08-10 12:59:12 -0300

    template de visualizacao de paginas

--- templates/admin/pages/index.tpl.php
+++ templates/admin/pages/index.tpl.php
@@ -1,3 +1,24 @@
-<h3>Administração de Páginas</h3>
+<h3 class="mb-5">Administração de Páginas</h3>
 
-listagem
+<table class="table table-hover">
+    <thead>
+        <tr>
+            <th>#</th>
+            <th>título</th>
+            <th></th>
+        </tr>
+    </thead>
+    <tbody>
+        <tr>
+            <td>1</td>
+            <td>
+                <a href="/admin/pages/1">Página Inicial</a>
+            </td>
+            <td class="text-right">
+                <a href="/admin/pages/1" class="btn btn-primary btn-sm">ver</a>
+            </td>
+        </tr>
+    </tbody>
+</table>
+
+<a class="btn btn-secondary" href="/admin/pages/create">Novo</a>
